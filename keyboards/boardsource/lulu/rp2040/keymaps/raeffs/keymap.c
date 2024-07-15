030a9f6d13cb617431a2a928090a223e46c355c0
Raphael Fleischlin <git@raeffs.dev>
2024-07-15 20:35:36 +0200

    define quertz layer

--- keyboards/boardsource/lulu/rp2040/keymaps/raeffs/keymap.c
+++ keyboards/boardsource/lulu/rp2040/keymaps/raeffs/keymap.c
@@ -2,7 +2,7 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-    _QWERTY,
+    _QWERTZ,
     _RAISE,
     _LOWER,
     _ADJUST
@@ -11,30 +11,34 @@ enum layers {
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 
+#define KC_AUML RALT(KC_A)
+#define KC_OUML RALT(KC_O)
+#define KC_UUML RALT(KC_U)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* QWERTY
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
- *                   |      |      |      |/       /         \      \ |      |      |      |
- *                   `----------------------------'           '------''--------------------'
- */
+/* QUERTZ
 
- [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                          KC_LALT,    KC_LGUI, LOWER, KC_SPC,  KC_ENT,   RAISE,   KC_BSPC, KC_RGUI
+ ,-----------------------------------------------------.                     ,-----------------------------------------------------.
+ |  ESC   |        |        |        |        |        |                     |        |        |        |        |        |        |
+ |--------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
+ |  TAB   |   Q    |   W    |   E    |   R    |   T    |                     |   Z    |   U    |   I    |   O    |   P    |   Ü    |
+ |--------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
+ | L CTRL |   A    |   S    |   D    |   F    |   G    |--------.   ,--------|   H    |   J    |   K    |   L    |   Ö    |   Ä    |
+ |--------+--------+--------+--------+--------+--------|        |   |        |--------+--------+--------+--------+--------+--------|
+ | L SFT  |   Y    |   X    |   C    |   V    |   B    |--------|   |--------|   N    |   M    |   ,    |   .    |   -    | R SFT  |
+ `-----------------------------------------------------/        /    \        \----------------------------------------------------'
+                         | L ALT  | L GUI  | LOWER  | / SPACE  /      \ ENTER  \ | RAISE  | BackSp | R GUI  |
+                         `------------------------------------'        '------------------------------------'
+*/
+[_QWERTZ] = LAYOUT(
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                          KC_Z,    KC_U,    KC_I,    KC_O,    KC_P,    KC_UUML,
+  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,    KC_OUML, KC_AUML,
+  KC_LSFT, KC_Y,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX,     XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_MINS, KC_RSFT,
+                             KC_LALT, KC_LGUI, LOWER,   KC_SPC,      KC_ENT,  RAISE,   KC_BSPC, KC_RGUI
 ),
+
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
